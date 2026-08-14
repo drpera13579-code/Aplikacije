8d7d2a46576123241a2bb523ef3d616a633dec23
drpera13579-code <drpera13579@gmail.com>
2026-08-14 13:11:46 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,15 @@
-const CACHE_NAME = 'licni-kalendar-smena-v1';
+const CACHE_NAME = 'licni-kalendar-smena-v2';
 const urlsToCache = [
     './licni-kalendar-smena.html',
     './manifest.json',
     './icon-512.png'
 ];
 
+// Instalacija - keširanje fajlova
 self.addEventListener('install', function (event) {
+    // NOVO: skipWaiting() - novi SW preuzima kontrolu odmah
+    self.skipWaiting();
+
     event.waitUntil(
         caches.open(CACHE_NAME)
             .then(function (cache) {
@@ -14,6 +18,14 @@ self.addEventListener('install', function (event) {
     );
 });
 
+// NOVO: Aktivacija - preuzimanje svih otvorenih tabova
+self.addEventListener('activate', function (event) {
+    event.waitUntil(
+        clients.claim()
+    );
+});
+
+// Fetch - odgovaranje na zahteve
 self.addEventListener('fetch', function (event) {
     event.respondWith(
         caches.match(event.request)
